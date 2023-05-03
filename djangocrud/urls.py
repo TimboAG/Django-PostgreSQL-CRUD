6e4e1c80f7275e542d98d10baea868fbca4661f2
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-05-02 21:19:27 -0300

    aprendiendo Django

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tareas import views
 
 urlpatterns = [
+    path('', views.lista_tareas, name= "vista_tareas"),
     path('admin/', admin.site.urls),
 ]
